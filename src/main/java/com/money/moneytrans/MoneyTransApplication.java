c46a7f177298fdeedaceb8578dc98214cdf1bcd7
mel mel <melmelina2555@gmail.com>
2023-04-12 08:45:01 +0300

    created git spring boot

---
+++ src/main/java/com/money/moneytrans/MoneyTransApplication.java
@@ -0,0 +1,13 @@
+package com.money.moneytrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/money/moneytrans/MoneyTransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.moneytrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
